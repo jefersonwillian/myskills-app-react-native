a6f741523ff945616c15c4c666bf33f92df53f9d
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2022-07-12 19:25:25 -0300

    PAGINA HOME

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export function Home() {
   return (
     <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+      <View style={styles.container}>
         <Text>Jeferson</Text>
         <Text>Carvalhod</Text>
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
